7ecdd5f58f479ff649f7fd89ff559b05294fac06
patilkunal07-shiv <patilkunal07@gmail.com>
2026-06-06 02:28:22 +0530

    Connect Gemini AI

--- api/generate.js
+++ api/generate.js
@@ -1,9 +1,54 @@
 export default async function handler(req, res) {
 
-  res.status(200).json({
-    success: true,
-    message: "NISM AI Engine Backend Working",
-    notes: "Backend connected successfully."
-  });
+  const apiKey = process.env.GEMINI_API_KEY;
+
+  if (!apiKey) {
+    return res.status(500).json({
+      error: "Gemini API Key Missing"
+    });
+  }
+
+  const { prompt } = req.body;
+
+  try {
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    const text =
+      data?.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "No response";
+
+    res.status(200).json({
+      result: text
+    });
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
 
 }
